0c1cd4a5ec766c31e6a7e1650f2f6aeaed79ba6c
Umanga Shrestha <umanga.907@gmail.com>
2025-07-01 16:35:43 +0545

    sidebar and container width fix to take full width

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -28,7 +28,7 @@ export function AppLayout({ children, pageTitle }: { children?: React.ReactNode;
       <div className="flex h-screen bg-background w-full">
         <DataToolsSidebar />
         <SidebarInset className="flex-1 flex flex-col min-h-0">
-          <main className="container mx-auto p-4 md:p-6 lg:p-8 flex flex-col flex-grow min-h-0">
+          <main className="w-full mx-auto p-4 md:p-6 lg:p-8 flex flex-col flex-grow min-h-0">
             {isAuthenticated && (
               <div className="flex-shrink-0"> {/* Header wrapper */}
                 <div className="flex flex-col gap-2"> {/* Vertical stacking for title block and context block */}
