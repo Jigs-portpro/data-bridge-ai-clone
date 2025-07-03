80e33cdf0865cd712dafa67100c696c84292fc36
Kushal Bajracharya <bajrakus20@gmail.com>
2025-07-03 16:32:33 +0545

    Update EntitySchemaLookupIds to include additional fields for 'Load Tariff', 'Driver Tariff', and 'Carrier Tariff'
    - Added lookup fields such as 'branches', 'tmsCustomers', 'containerTypes', 'containerSizes', 'containerOwners', 'CSR', 'chassisTypes', 'chassisSizes', and 'chassisOwners' to the 'Load Tariff', 'Driver Tariff', and 'Carrier Tariff' schemas for improved data integrity and validation.

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -817,7 +817,17 @@ const EntitySchemaLookupIds: Record<string, string[]> = {
   Carrier: [
     'branches'
   ],
-  'Load Tariff': [],
+  'Load Tariff': [
+    'branches',
+    'tmsCustomers',
+    'containerTypes',
+    'containerSizes',
+    'containerOwners',
+    'CSR',
+    'chassisTypes',
+    'chassisSizes',
+    'chassisOwners'
+  ],
   Trailers: [
     'branches'
   ],
@@ -876,7 +886,17 @@ const EntitySchemaLookupIds: Record<string, string[]> = {
     'zipcodeGroups',
     'driverGroups'
   ],
-  'Driver Tariff': [],
+  'Driver Tariff': [
+    'branches',
+    'tmsCustomers',
+    'containerTypes',
+    'containerSizes',
+    'containerOwners',
+    'CSR',
+    'chassisTypes',
+    'chassisSizes',
+    'chassisOwners'
+  ],
   Drivers: [
     'trucks',
     'driverProfileTypes',
@@ -899,6 +919,17 @@ const EntitySchemaLookupIds: Record<string, string[]> = {
     'zipcodeGroups',
     'carrierGroups'
   ],
+  'Carrier Tariff': [
+    'branches',
+    'tmsCustomers',
+    'containerTypes',
+    'containerSizes',
+    'containerOwners',
+    'CSR',
+    'chassisTypes',
+    'chassisSizes',
+    'chassisOwners'
+  ] 
 };
 
 export {
